8702cb8797fcfa32615cc5b04d1a6760ccf72ec5
abu-said <abubdcalling@gmail.com>
2025-02-04 11:12:41 +0600

    npm i react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-1xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
 }
